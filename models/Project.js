0aeff23079ccd94d4222cfde76d600dd2f97237b
lezendarynoob <lezendarynoob@gmail.com>
2020-06-04 13:31:09 +0530

    Project Schema added

---
+++ models/Project.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const ProjectSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    createdBy: {type: ObjectId, required: true},
+    universities: {type: [String]},
+    minQual: {type: String},
+    countries: {type: [String]},
+    minAge: {type: Number},
+    maxAge: {type: Number},
+    skills: {type: [String]}
+});
+
+const Project = mongoose.model('Project', ProjectSchema);
+
+module.exports = Project;
